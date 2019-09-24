2c65986e1292ec0dc4e5f16cfbedfba906c30864
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-09-24 12:34:16 -0500

    Fixing.

--- passagenwerk/Segments/Segment_I/definition.py
+++ passagenwerk/Segments/Segment_I/definition.py
@@ -15,6 +15,28 @@ from passagenwerk.Materials.pitch.Segment_I.clef_handlers import clef_handlers
 
 maker = evans.SegmentMaker(
         instruments=insts,
+        names=[
+            "Violin I-1",
+            "Violin I-2",
+            "Violin II-1",
+            "Violin II-2",
+            "Viola-1",
+            "Viola-2",
+            "Violoncello-1",
+            "Violoncello-2",
+            "Contrabass",
+        ],
+        abbreviations=[
+            "vln. I-1",
+            "vln. I-2",
+            "vln. II-1",
+            "vln. II-2",
+            "vla.-1",
+            "vla.-2",
+            "vc.-1",
+            "vc.-2",
+            "cb.",
+        ],
         rhythm_timespans=segment_I_rhythm_timespans,
         handler_timespans=segment_I_timespans,
         score_template=score,
