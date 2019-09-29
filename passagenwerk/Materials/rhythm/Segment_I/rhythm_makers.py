79092101fa00b7598f92a344b87eb9e57f7ae3f2
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-09-28 21:11:58 -0500

    Black formatted.

--- passagenwerk/Materials/rhythm/Segment_I/rhythm_makers.py
+++ passagenwerk/Materials/rhythm/Segment_I/rhythm_makers.py
@@ -8,10 +8,8 @@ from passagenwerk.Materials.rhythm.Segment_I.rtm_lists import final_rtm_list
 
 rmaker_one = abjadext.rmakers.stack(
     abjadext.rmakers.talea(
-        [2, 1, 6, 4, 3, 5],
-        16,
-        extra_counts=[0, -1, 0, 1, -1, 0, 1],
-        ),
+        [2, 1, 6, 4, 3, 5], 16, extra_counts=[0, -1, 0, 1, -1, 0, 1]
+    ),
     abjadext.rmakers.trivialize(abjad.select().tuplets()),
     abjadext.rmakers.extract_trivial(abjad.select().tuplets()),
     abjadext.rmakers.rewrite_rest_filled(abjad.select().tuplets()),
@@ -25,18 +23,14 @@ rmaker_one = abjadext.rmakers.stack(
     #         abjad.select()[0]
     #         )
     #     ),
-    )
+)
 
 
 rmaker_two = evans.RTMMaker(rtm=final_rtm_list)
 
 
 rmaker_three = abjadext.rmakers.stack(
-    abjadext.rmakers.talea(
-        [3, 2, 4, 1, 5],
-        8,
-        extra_counts=[0, 1, -1, 1, 0, -1],
-        ),
+    abjadext.rmakers.talea([3, 2, 4, 1, 5], 8, extra_counts=[0, 1, -1, 1, 0, -1]),
     abjadext.rmakers.trivialize(abjad.select().tuplets()),
     abjadext.rmakers.extract_trivial(abjad.select().tuplets()),
     abjadext.rmakers.rewrite_rest_filled(abjad.select().tuplets()),
@@ -50,7 +44,7 @@ rmaker_three = abjadext.rmakers.stack(
     #         abjad.select()[0]
     #         )
     #     ),
-    )
+)
 
 
 rmaker_four = abjadext.rmakers.stack(
@@ -58,7 +52,7 @@ rmaker_four = abjadext.rmakers.stack(
         [3, 5, 7, 5, 3, 7, 3, 7, 5, 7, 5, 3, 5, 7, 3, 7, 3, 5],
         8,
         extra_counts=[0, 1, 0, -1],
-        ),
+    ),
     abjadext.rmakers.trivialize(abjad.select().tuplets()),
     abjadext.rmakers.extract_trivial(abjad.select().tuplets()),
     abjadext.rmakers.rewrite_rest_filled(abjad.select().tuplets()),
@@ -72,15 +66,13 @@ rmaker_four = abjadext.rmakers.stack(
     #         abjad.select()[2]
     #         )
     #     ),
-    )
+)
 
 
 rmaker_five = abjadext.rmakers.stack(
     abjadext.rmakers.talea(
-        [2, 1, 1, 3, 2, 4, 2, 1, 4, 12],
-        8,
-        extra_counts=[1, 0, -1, 1, 0, 1, -1],
-        ),
+        [2, 1, 1, 3, 2, 4, 2, 1, 4, 12], 8, extra_counts=[1, 0, -1, 1, 0, 1, -1]
+    ),
     abjadext.rmakers.trivialize(abjad.select().tuplets()),
     abjadext.rmakers.extract_trivial(abjad.select().tuplets()),
     abjadext.rmakers.rewrite_rest_filled(abjad.select().tuplets()),
@@ -94,14 +86,13 @@ rmaker_five = abjadext.rmakers.stack(
     #         abjad.select()[3]
     #         )
     #     ),
-    )
+)
 
 
 rmaker_six = abjadext.rmakers.stack(
     abjadext.rmakers.even_division(
-        [16, 16, 8, 16, 16],
-        extra_counts=[0, 1, -1, 0, 1, 0, -1]
-        ),
+        [16, 16, 8, 16, 16], extra_counts=[0, 1, -1, 0, 1, 0, -1]
+    ),
     abjadext.rmakers.trivialize(abjad.select().tuplets()),
     abjadext.rmakers.extract_trivial(abjad.select().tuplets()),
     abjadext.rmakers.rewrite_rest_filled(abjad.select().tuplets()),
@@ -115,4 +106,4 @@ rmaker_six = abjadext.rmakers.stack(
     #         abjad.select()[0]
     #         )
     #     ),
-    )
+)
